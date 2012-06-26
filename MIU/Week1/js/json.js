f03db56381a1dacff900f71871a8c90cf57aba42
Jason Bentley <jbxfade@fullsail.edu>
2012-06-25 18:47:30 -0700

    Meaningful commit1

---
+++ MIU/Week1/js/json.js
@@ -0,0 +1,31 @@
+//Jason Bentley
+//VFW 1206 
+//Project 4
+
+var json = {
+	"Recipe1" : {
+		"recipename": ["Recipe Name: ", "Chicken Crescents"] ,
+		"groups": ["Group: ", "Chicken"],
+		"rating": ["Rating: ", "10"],
+		"date": ["Date Added: ", "2011-6-17"],
+		"checks": ["Meal Time: ", ["dinner"]],
+		"directions": ["Directions: ", "Cook some chicken"]
+	},
+	"Recipe2" : {
+		"recipename": ["Recipe Name: ", "Big Bob's Ribs"],
+		"groups": ["Group: ", "Beef"],
+		"rating": ["Rating: ", "8"],
+		"date": ["Date Added: ", "2011-4-27"],
+		"checks": ["Meal Time: ", ["dinner"]],
+		"directions": ["Directions: ", "Cook some Ribs"]
+	},
+	"Recipe3" : {
+		"recipename": ["Recipe Name: ", "Momlettes"],
+		"groups": ["Group: ", "Veggie"],
+		"rating": ["Rating: ", "10"],
+		"date": ["Date Added: ", "2010-3-04"],
+		"checks": ["Meal Time: ", ["breakfast"]],
+		"directions": ["Directions: ", "Cook some omlets"]
+	}
+
+}
